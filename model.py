40085395185f34177f83f28ce63d6468b693e73c
unknown <naufalhmuzakki@gmail.com>
2025-11-25 17:35:44 +0700

    fix

--- model.py
+++ model.py
@@ -1,15 +1,14 @@
 import cv2
 import matplotlib.pyplot as plt
-import numpy as np
+import skimage.io
 
-img_rgb = cv2.imread('dataset/42_low.png')
-print(img_rgb)
+img_gbr = skimage.io.imread('dataset/642.png')
 
-img_ycbcr = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2YCrCb)
+img_ycbcr = cv2.cvtColor(img_gbr, cv2.COLOR_RGB2YCrCb)
 Y = img_ycbcr[:, :, 0]
-clahe = cv2.createCLAHE(clipLimit=5)
-clahe_img = np.clip(clahe.apply(Y) + 30, 0, 255).astype(np.uint8)
+clahe = cv2.createCLAHE(clipLimit=4)
+clahe_img = clahe.apply(Y)
 img_ycbcr[:, :, 0] = clahe_img
 img_rgb_processed = cv2.cvtColor(img_ycbcr, cv2.COLOR_YCrCb2RGB)
-cv2.imshow("test", img_rgb_processed)
-cv2.waitKey(0)
+plt.imshow(img_rgb_processed)
+plt.show()

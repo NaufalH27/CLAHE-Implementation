64203e4be52eda59a34e7dfc24581404e0b53c14
unknown <naufalhmuzakki@gmail.com>
2025-11-28 09:47:48 +0700

    remove clahe using built in library, and add naive ahe and also gaussian ahe image processing script

---
+++ gaussian_ahe.py
@@ -0,0 +1,67 @@
+# ahe menggunakan gaussian, gambar 2
+
+import cv2
+import numpy as np
+import skimage.io
+import matplotlib.pyplot as plt
+
+img_rgb = skimage.io.imread('dataset/225_low.png')
+img_ycbcr = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2YCrCb)
+Y = img_ycbcr[:, :, 0]
+
+def he(image, L=256):
+    hist, _ = np.histogram(image, bins=L, range=(0,L))
+    cdf = np.cumsum(hist) / image.size
+    lut = np.floor((L-1) * cdf).astype(np.uint8)
+    return lut[image]
+
+def extract_patches(img, patch_size, overlap):
+    h, w = img.shape
+    patches = []
+    coords = []
+
+    step = patch_size - overlap
+    
+    for i in range(0, h, step):
+        for j in range(0, w, step):
+            i_end = min(i + patch_size, h)
+            j_end = min(j + patch_size, w)
+            patch = img[i:i_end, j:j_end]
+            patches.append(patch)
+            coords.append((i, j))
+    return patches, coords
+
+def gaussian_merge(patches, coords, img_shape, patch_size, overlap):
+    h, w = img_shape
+    result = np.zeros((h,w), np.float32)
+    weight_sum = np.zeros((h,w), np.float32)
+
+    sigma = patch_size/3
+    ax = np.linspace(-1,1,patch_size)
+    gauss = np.exp(- (ax**2)/(2*(sigma/patch_size)**2))
+    kernel = np.outer(gauss,gauss)
+    
+    step = patch_size - overlap
+
+    for (patch,(i,j)) in zip(patches,coords):
+        ph,pw = patch.shape
+        
+        k = kernel[:ph,:pw]
+
+        result[i:i+ph,j:j+pw] += patch.astype(np.float32) * k
+        weight_sum[i:i+ph,j:j+pw] += k
+
+    return (result/weight_sum).astype(np.uint8)
+
+
+patch_size = 64
+overlap = 32  
+
+patches, coords = extract_patches(Y, patch_size, overlap)
+he_patches = [he(p,256) for p in patches]
+
+ahe_gaussian = gaussian_merge(he_patches, coords, Y.shape, patch_size, overlap)
+
+plt.imshow(ahe_gaussian, cmap="gray")
+plt.axis('off')
+plt.show()

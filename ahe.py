3acb2ffb9e402b21bcda0387be555ec5f7f03785
unknown <naufalhmuzakki@gmail.com>
2025-11-29 07:08:38 +0700

    change from gaussian to bilinear:

--- ahe.py
+++ ahe.py
@@ -27,38 +27,37 @@ def extract_patches(img, patch_size, overlap):
             coords.append((i, j))
     return patches, coords
 
-def gaussian_merge(patches, coords, img_shape, patch_size, overlap):
+def bilinear_merge(patches, coords, img_shape, patch_size, overlap):
     h, w = img_shape
-    result = np.zeros((h,w), np.float32)
-    weight_sum = np.zeros((h,w), np.float32)
+    result = np.zeros((h, w), np.float32)
+    weight_sum = np.zeros((h, w), np.float32)
 
-    sigma = patch_size/3
-    ax = np.linspace(-1,1,patch_size)
-    gauss = np.exp(- (ax**2)/(2*(sigma/patch_size)**2))
-    kernel = np.outer(gauss,gauss)
-    
+    # Create bilinear kernel
+    ax = np.linspace(0, 1, patch_size)
+    bx = 1 - np.abs(ax - 0.5) * 2
+    kernel = np.outer(bx, bx) 
+    kernel /= kernel.max()
+
     step = patch_size - overlap
 
-    for (patch,(i,j)) in zip(patches,coords):
-        ph,pw = patch.shape
-        
-        k = kernel[:ph,:pw]
+    for patch, (i, j) in zip(patches, coords):
+        ph, pw = patch.shape
+        k = kernel[:ph, :pw]
+        result[i:i+ph, j:j+pw] += patch.astype(np.float32) * k
+        weight_sum[i:i+ph, j:j+pw] += k
 
-        result[i:i+ph,j:j+pw] += patch.astype(np.float32) * k
-        weight_sum[i:i+ph,j:j+pw] += k
-
-    return (result/weight_sum).astype(np.uint8)
+    return np.clip(result / weight_sum, 0, 255).astype(np.uint8)
 
 
-def gaussian_ahe(image, L=256, patch_size=64, overlap=32):
+def ahe(image, L=256, patch_size=64, overlap=32):
     patches, coords = extract_patches(image, patch_size, overlap)
     he_patches = [he(p,L) for p in patches]
-    return gaussian_merge(he_patches, coords, image.shape, patch_size, overlap)
+    return bilinear_merge(he_patches, coords, image.shape, patch_size, overlap)
 
 img_rgb = skimage.io.imread('dataset/225_low.png')
 img_ycbcr = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2YCrCb)
 Y = img_ycbcr[:, :, 0]
 
-plt.imshow(gaussian_ahe(Y), cmap="gray")
+plt.imshow(ahe(Y), cmap="gray")
 plt.axis('off')
 plt.show()
--- clahe.py
+++ clahe.py
@@ -21,29 +21,29 @@ def extract_patches(img, patch_size, overlap):
             coords.append((i, j))
     return patches, coords
 
-def gaussian_merge(patches, coords, img_shape, patch_size, overlap):
+def bilinear_merge(patches, coords, img_shape, patch_size, overlap):
     h, w = img_shape
-    result = np.zeros((h,w), np.float32)
-    weight_sum = np.zeros((h,w), np.float32)
+    result = np.zeros((h, w), np.float32)
+    weight_sum = np.zeros((h, w), np.float32)
 
-    sigma = patch_size / 2
-    ax = np.arange(patch_size) - patch_size//2
-    gauss = np.exp(-(ax**2)/(2*(sigma**2)))
-    kernel = np.outer(gauss, gauss)
+    # Create bilinear kernel
+    ax = np.linspace(0, 1, patch_size)
+    bx = 1 - np.abs(ax - 0.5) * 2
+    kernel = np.outer(bx, bx) 
     kernel /= kernel.max()
 
     step = patch_size - overlap
 
-    for (patch,(i,j)) in zip(patches,coords):
-        ph,pw = patch.shape
-        k = kernel[:ph,:pw]
-        result[i:i+ph,j:j+pw] += patch.astype(np.float32) * k
-        weight_sum[i:i+ph,j:j+pw] += k
+    for patch, (i, j) in zip(patches, coords):
+        ph, pw = patch.shape
+        k = kernel[:ph, :pw]
+        result[i:i+ph, j:j+pw] += patch.astype(np.float32) * k
+        weight_sum[i:i+ph, j:j+pw] += k
 
-    return np.clip(result/weight_sum, 0, 255).astype(np.uint8)
+    return np.clip(result / weight_sum, 0, 255).astype(np.uint8)
 
 
-def gaussian_clahe(image, alpha, smax, L=256, patch_size=32, overlap=25):
+def clahe(image, alpha, smax, L=256, patch_size=32, overlap=25):
     patches, coords = extract_patches(image, patch_size, overlap)
     for i, p in enumerate(patches):
         h,w = p.shape
@@ -57,17 +57,29 @@ def gaussian_clahe(image, alpha, smax, L=256, patch_size=32, overlap=25):
         lut = np.floor((L-1) * cdf).astype(np.uint8)
         patches[i] = lut[p]
 
-    return gaussian_merge(patches, coords, image.shape, patch_size, overlap)
+    return bilinear_merge(patches, coords, image.shape, patch_size, overlap)
 
 
-img_rgb = skimage.io.imread('dataset/642.png')
+img_rgb = skimage.io.imread('dataset/225_low.png')
 img_ycbcr = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2YCrCb)
 Y = img_ycbcr[:, :, 0]
 
-Y_clahe = gaussian_clahe(Y, alpha=60, smax=10, patch_size=64, overlap=60)
+Y_clahe = clahe(Y, alpha=200, smax=3, patch_size=64, overlap=32)
 img_ycbcr[:, :, 0] = Y_clahe
 img = cv2.cvtColor(img_ycbcr, cv2.COLOR_YCrCb2RGB)
 
-plt.imshow(img)
-plt.axis('off')
+fig, ax = plt.subplots(1, 3, figsize=(10, 5), gridspec_kw={'wspace': 0.02})
+
+ax[0].imshow(img)
+ax[0].axis("off")
+ax[0].text(0.5, -0.05, "(a)", ha='center', va='top', transform=ax[0].transAxes, fontsize=14)
+
+ax[1].imshow(Y_clahe, cmap='gray')
+ax[1].axis("off")
+ax[1].text(0.5, -0.05, "(b)", ha='center', va='top', transform=ax[1].transAxes, fontsize=14)
+
+ax[2].imshow(img_rgb)
+ax[2].axis("off")
+ax[2].text(0.5, -0.05, "(c)", ha='center', va='top', transform=ax[2].transAxes, fontsize=14)
+
 plt.show()
